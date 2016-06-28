7f964b4d9257a2abbc790be6475709df03b9d1fd
Adam <crappyoats@gmail.com>
2016-06-28 13:02:47 -0500

    added error emailing

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 from interface import Interface
+import sys
+import traceback
+import smtplib
+
+
+def mail(trace):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+
+    info = open('info.txt', 'r')
+    login_info = [x.strip('\n') for x in info.readlines()]
+    server.login(login_info[0], login_info[1])
+
+    msg = traceback.format_exc(trace)
+    server.sendmail("crappyoats@gmail.com", "vilmin2@illinois.edu", msg)
+    server.quit()
+
 
 if __name__ == '__main__':
-    interface = Interface()
-    interface.start()
+    try:
+        interface = Interface()
+        interface.start()
+    except:
+        ex_type, ex, tb = sys.exc_info()
+        mail(tb)
+    finally:
+        del tb
