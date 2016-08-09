340025c1a4375fb7b32cb25f0b58b4693b024fd2
Adam <crappyoats@gmail.com>
2016-08-09 17:40:59 -0500

    remove email feature

--- eclipse_vision/main.py
+++ eclipse_vision/main.py
@@ -1,20 +1,4 @@
 from interface import Interface
-import sys
-import traceback
-import smtplib
-
-
-def mail(trace):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-
-    info = open('info.txt', 'r')
-    login_info = [x.strip('\n') for x in info.readlines()]
-    server.login(login_info[0], login_info[1])
-
-    msg = traceback.format_exc(trace)
-    server.sendmail("crappyoats@gmail.com", "vilmin2@illinois.edu", msg)
-    server.quit()
 
 
 if __name__ == '__main__':
