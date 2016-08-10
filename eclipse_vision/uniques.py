1e6ebf17356622f42466b9724e2421ea8ca4271c
Adam <crappyoats@gmail.com>
2016-08-10 13:24:56 -0500

    doxxxxxxxxx

--- eclipse_vision/uniques.py
+++ eclipse_vision/uniques.py
@@ -1,9 +1,13 @@
+# Adam Vilmin
+# Illinois State Geological Survey, University of Illinois
+# 2015-05-31
+
 from collections import defaultdict
 from sys import argv
 
 
 def uniques(rf, wf):
-
+    """script for finding searchable values in PRT file"""
     types = defaultdict(int)
 
     for line in rf:
@@ -12,12 +16,11 @@ def uniques(rf, wf):
             types[line] += 1
 
     for key, value in types.items():
-        wf.write('"'+ key +'"' + " occurs "+ str(value) + " times.\n")
+        wf.write('"' + key + '"' + " occurs " + str(value) + " times.\n")
 
 if __name__ == '__main__':
-    
+
     rf = open(argv[1], 'r')
     wf = open(argv[2], 'w')
 
     uniques(rf, wf)
-    
